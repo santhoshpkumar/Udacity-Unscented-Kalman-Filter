b2cf0a565d241dfe6016c50c9fdb04ad78fe996e
Santhosh Kumar <santhosh.prakash@gmail.com>
2018-09-08 07:37:44 +0530

    Unscented kalman filter implementation

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,38 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  // Input validation
+  if(estimations.size() == 0){
+    cout << "ERROR - CalculateRMSE () - The estimations vector is empty" << endl;
+    return rmse;
+  }
+
+  if(ground_truth.size() == 0){
+    cout << "ERROR - CalculateRMSE () - The ground-truth vector is empty" << endl;
+    return rmse;
+  }
+
+  unsigned int n = estimations.size();
+  if(n != ground_truth.size()){
+    cout << "ERROR - CalculateRMSE () - The ground-truth and estimations vectors must have the same size." << endl;
+    return rmse;
+  }
+
+  // Calculate the resuidal
+  for(unsigned int i=0; i < estimations.size(); ++i){
+    VectorXd residuals = estimations[i] - ground_truth[i];
+    residuals = residuals.array() * residuals.array();
+    rmse += residuals;
+  }
+
+  //calculate the mean
+  rmse = rmse / n;
+  //calculate the squared root
+  rmse = rmse.array().sqrt();
+  //return the result
+  return rmse;
 }
